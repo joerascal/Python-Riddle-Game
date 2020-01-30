993daeda8d3720d2f61451715a3c4604668460b7
joerascal <43782608+joerascal@users.noreply.github.com>
2020-01-30 02:05:04 +0000

    Update Riddle.py
    remaining lives incorporated into this little game

--- Riddle.py
+++ Riddle.py
@@ -1,21 +1,49 @@
-answer = input("What is the largest city in the United Kindgom?")
+age = int(input("How old are you? "))
 
-if answer == "London":
-	
-	print("Correct answer")
+if age >= 18:
 
-if answer == " London":
+	lives = 3
+	while lives >= 1:
 
-	print("Correct answer")
+		answer = input("What is the largest city in the United Kindgom? ")
 
-if answer == "london":
+		if answer == "London":
+			
+			print("Correct answer")
+			break
 
-	print("Correct answer")
+		elif answer == " London":
 
-if answer == " london":
-	
-	print("Correct answer")
+			print("Correct answer")
+			break
 
+		elif answer == "london":
+
+			print("Correct answer")
+			break
+
+		elif answer == " london":
+			
+			print("Correct answer")
+			break
+
+		else:
+
+			lives = lives - 1
+
+			if lives == 1:
+
+				print("Wrong answer, careful now you have ONLY " + str(lives) + " life left!!!")
+			
+			elif lives == 0:
+
+				print("GAME OVEREEEEERRRRRRRRRRR ^_^")
+
+			else:
+
+				print("Wrong answer, you have " + str(lives) + " lives left")
 else:
 
-	print("Wrong answer")
+	print("You need to be 18 or over to play this game.")
+
+print("Thanks for playing")
